83ed82e3f9ed57c202d18a9cb22be6019d9e1561
rohitumadi <rohitumadi2000@gmail.com>
2024-01-27 09:54:32 +0530

    added form validations

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
-
+  overflow: scroll;
   padding: 4rem 4.8rem 6.4rem;
 `;
 
@@ -16,13 +16,23 @@ const StyledAppLayout = styled.div`
   grid-template-rows: auto 1fr;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
