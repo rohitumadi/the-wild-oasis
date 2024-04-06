90633ce2de01563e023a4bee74ed373cdf739219
rohitumadi <rohitumadi2000@gmail.com>
2024-04-06 19:35:11 +0530

    signup n login done

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,13 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useSignUp() {
+  const { mutate: signup, isLoading } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (user) => {
+      toast.success("User created successfully");
+    },
+  });
+  return { signup, isLoading };
+}
